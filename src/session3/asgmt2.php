c46d4255e47c67ee5a9010704b2907144ddffdf2
Antoni Kania <antek.kania@wp.pl>
2023-04-17 20:45:10 +0200

    site visits counter

---
+++ src/session3/asgmt2.php
@@ -0,0 +1,20 @@
+<?php
+$filename = 'licznik.txt';
+
+if (!file_exists($filename)) {
+    $file = fopen($filename, 'w');
+    fwrite($file, '1');
+    $visits = 1;
+    fclose($file);
+} else {
+    $file = fopen($filename, 'r+');
+    $visits = intval(fgets($file)) + 1;
+
+    rewind($file);
+    fwrite($file, $visits);
+
+    fclose($file);
+}
+
+echo 'Number of visits ' . $visits;
+?>
