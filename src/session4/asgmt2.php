a3cc0b753e1ba713bd54632093e242055e2feceb
AntoniKania <antek.kania@wp.pl>
2023-05-12 22:07:58 +0200

    destroying cookies with session end

--- src/session4/asgmt2.php
+++ src/session4/asgmt2.php
@@ -4,7 +4,7 @@ $visits = isset($_COOKIE['visits']) ? $_COOKIE['visits'] : 0;
 $givenNumber = 5;
 
 if ($visits >= $givenNumber) {
-  echo "Congrats, this is hidden site that is visible after $givenNumber number of visits";
+  echo "Congrats, this is a hidden site that is visible after $givenNumber number of visits";
 } else {
   $visits++;
   setcookie('visits', $visits, time() + (60 * 60));
